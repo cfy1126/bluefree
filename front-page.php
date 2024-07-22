9c9d4a91c971180a463e4e335839833b927f59ba
曹发友(80016865) <fayou.cao@hxgroup.com>
2024-07-22 10:00:05 +0800

    在模板中动态加载functions.php的脚本资源

--- front-page.php
+++ front-page.php
@@ -10,7 +10,7 @@
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">
     <link rel="shortcut icon" href="images/logo.png">
-    <!-- 动态加加载functions.php的资源 -->
+    <!-- 动态加加载functions.php的样式表资源 -->
     <?php wp_head(); ?>
 
 </head>
@@ -125,13 +125,8 @@
 
     </div>
 
-
-    <!-- Bootstrap Javascript -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="js/main.js"></script>
-
+    <!-- 动态加载functions.php脚本资源 -->
+    <?php wp_footer(); ?>
 </body>
 
 </html>
--- functions.php
+++ functions.php
@@ -1,9 +1,22 @@
 <?php
 function bluefree_register_styles()
 {
-    wp_enqueue_style('bluefree-style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
-    wp_enqueue_style('bluefree-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '1.0', 'all');
-    wp_enqueue_style('bluefree-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css', array(), '1.0', 'all');
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('bluefree-style', get_template_directory_uri() . '/style.css', array('bluefree-bootstrap'), $version, 'all');
+    wp_enqueue_style('bluefree-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '4.4.1', 'all');
+    wp_enqueue_style('bluefree-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css', array(), '5.13.0', 'all');
 }
 
 add_action("wp_enqueue_scripts", "bluefree_register_styles");
+
+
+function bluefree_register_scripts()
+{
+    wp_enqueue_script('bluefree-jquery','https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
+    wp_enqueue_script('bluefree-popper','https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
+    wp_enqueue_script('bluefree-bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true);
+    wp_enqueue_script('bluefree-main', get_template_directory_uri().'/assets/javascript/main.js', array(), '1.0', true);
+
+}
+
+add_action("wp_enqueue_scripts", "bluefree_register_scripts");
